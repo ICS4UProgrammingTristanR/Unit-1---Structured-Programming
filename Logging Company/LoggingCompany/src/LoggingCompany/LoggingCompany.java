d6a47f6c37e1884426f56e9400ce62686980169e
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2019-09-09 13:10:05 -0400

    Update LoggingCompany.java

--- Logging Company/LoggingCompany/src/LoggingCompany/LoggingCompany.java
+++ Logging Company/LoggingCompany/src/LoggingCompany/LoggingCompany.java
@@ -1,15 +1,22 @@
 package LoggingCompany;
 
+import java.util.Scanner;
 public class LoggingCompany {
-	
+	   public static void main(String args[]) {
 
 	// declare variables
-	const float MAX_WEIGHT = 1100;
-	const double PREMIER_LENGTH = 0.25;
-	const double SECOND_LENGTH = 0.5;
-	const double FINAL_LENGTH = 1;
-	
+     Scanner input = new scanner (System.in);
+     double value = scan.string_prompt();
+	 double MAX_WEIGHT = 1100;
+	 double PREMIER_LENGTH = 0.25;
+	 double SECOND_LENGTH = 0.5;
+	 double FINAL_LENGTH = 1;
+	 int count = in.readLine();
+	 double weight = PREMIER_LENGTH * 20;
+	 double number_Of_Logs = 1100 / weight;
+	 System.out.println(number_Of_Logs);
+
 
 }
-
+}
 
