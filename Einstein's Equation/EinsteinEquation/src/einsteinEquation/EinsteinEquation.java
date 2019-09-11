7e2bad937e3675f8f4ef21de99b36cc1e5d59d6d
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2019-09-11 13:11:51 -0400

    ok

--- Einstein's Equation/EinsteinEquation/src/einsteinEquation/EinsteinEquation.java
+++ Einstein's Equation/EinsteinEquation/src/einsteinEquation/EinsteinEquation.java
@@ -1,13 +1,17 @@
 package einsteinEquation;
 import java.util.Scanner;
-import java.io.InputStream;
+
 
 public class EinsteinEquation {
 private static Scanner scanner = new Scanner( System.in );
 
 	public static void main(String args[]) {
 	System.out.print("What is the mass of your object?");
-	double mass = scanner.nextDouble();
-	double speedOfLight = 2.998x10 
+	double m = scanner.nextDouble();
+	double speedOfLight = (double) Math.pow(10,8);
+	double c = 2.998 * speedOfLight;
+	double cSquared = Math.pow(c ,2);
+	double e = m * cSquared;
+	System.out.println("The equation resulted in:" + e);
 }
 }
